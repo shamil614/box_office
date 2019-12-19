f212727c2def32b21a0ca19f3fbfd9e5bdf22847
Scott Hamilton <scott.hamilton@dockyard.com>
2019-12-19 15:39:53 -0600

    Basic setup of GenStateMachine

--- lib/show_seat_state.ex
+++ lib/show_seat_state.ex
@@ -8,9 +8,8 @@ defmodule BoxOffice.ShowSeatState do
     %ShowSeat{current_state: current_state} = show_seat
 
     default_state = Keyword.get(opts, :default_state, :available)
-    state_timeout = Keyword.get(opts, :state_timeout, 5000)
 
-    data = %{default_state: default_state, state_timeout: state_timeout, show_seat: show_seat}
+    data = %{default_state: default_state, show_seat: show_seat}
 
     GenStateMachine.start_link(__MODULE__, {current_state, data})
   end
@@ -39,18 +38,10 @@ defmodule BoxOffice.ShowSeatState do
     GenStateMachine.call(pid, {:hold, customer})
   end
 
-  @doc """
-  Purchase a seat for a customer.
-  """
-  def purchase(pid, customer = %Customer{}) do
-    GenStateMachine.call(pid, {:purchase, customer})
-  end
-
   ### Server API
 
   @doc """
   State can transition from `available` to `held`.
-  A timeout is set to reset the state.
   """
   def handle_event({:call, from}, {:hold, customer}, :available, data) do
     %{state_timeout: state_timeout} = data
@@ -58,49 +49,7 @@ defmodule BoxOffice.ShowSeatState do
     data =
       data
       |> Map.put(:current_customer, customer)
-
-    {:next_state, :held, data, [{:reply, from, {:ok, :held}}, {:state_timeout, state_timeout, :hold_timeout}]}
-  end
-
-  @doc """
-  Can't transition from `purchased` to `held`.
-  """
-  def handle_event({:call, from}, {:hold, _customer}, :purchased, _data) do
-    reason = {:no_transition, %{from: :held, to: :purchased}}
-
-    {:keep_state_and_data, [{:reply, from, {:error, reason}}]}
-  end
-
-  @doc """
-  State can transition from `held` to `purchased`.
-  """
-  def handle_event({:call, from}, {:purchase, customer}, :held, data) do
-    %Customer{id: customer_id} = customer
-    %{current_customer: %Customer{id: current_customer_id}} = data
-
-    if customer_id == current_customer_id do
-      data =
-        data
-        |> Map.put(:current_customer, customer)
-
-      {:next_state, :purchased, data, [{:reply, from, {:ok, :purchased}}]}
-    else
-      reason = {:no_transition, :unavailable}
-      {:keep_state_and_data, [{:reply, from, {:error, reason}}]}
-    end
-  end
-
-  @doc """
-  Timeout is triggered when the current state is `held`.
-  State resets to the `default_state`.
-  """
-  def handle_event(:state_timeout, :hold_timeout, :held, data) do
-    %{default_state: default_state} = data
-
-    data =
-      data
-      |> Map.put(:current_customer, nil)
-
-    {:next_state, default_state, data}
+ 
+     {:next_state, :held, data, [{:reply, from, {:ok, :held}}]}
   end
 end
--- test/show_seat_state_test.exs
+++ test/show_seat_state_test.exs
@@ -17,7 +17,7 @@ defmodule BoxOffice.ShowSeatStateTest do
     full_state = ShowSeatState.get_state(pid)
 
     assert full_state ==
-             {:available, %{default_state: :available, state_timeout: 5_000, show_seat: show_seat}}
+             {:available, %{default_state: :available, show_seat: show_seat}}
   end
 
   test "spawn a process to track the current state", context do
@@ -31,7 +31,7 @@ defmodule BoxOffice.ShowSeatStateTest do
   test "holds a seat for a set interval and resets state on timeout", context do
     %{show_seat: show_seat, customer: customer} = context
 
-    {:ok, pid} = ShowSeatState.start_link(show_seat, state_timeout: 1_000)
+    {:ok, pid} = ShowSeatState.start_link(show_seat)
 
     assert ShowSeatState.current_state(pid) == :available
 
@@ -40,93 +40,6 @@ defmodule BoxOffice.ShowSeatStateTest do
 
     Process.sleep(1_001)
 
-    assert {:available, %{current_customer: nil}} = ShowSeatState.get_state(pid)
-  end
-
-  test "seat can transition from hold to purchased, seat can't be held after purchase", context do
-    %{show_seat: show_seat, customer: customer} = context
-
-    {:ok, pid} = ShowSeatState.start_link(show_seat, state_timeout: 200)
-
-    assert ShowSeatState.current_state(pid) == :available
-
-    assert ShowSeatState.hold(pid, customer) == {:ok, :held}
-    assert {:held, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-
-    # state stays held before timeout is triggered
-    Process.sleep(100)
-    assert {:held, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-
-    # state can transition from held to purchased
-    assert ShowSeatState.purchase(pid, customer) == {:ok, :purchased}
-    assert {:purchased, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-
-    # seat stays purchased after the timeout
-    Process.sleep(300)
-    assert {:purchased, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-
-    # seat can't go back to hold
-    assert ShowSeatState.hold(pid, customer) == {:error, {:no_transition, %{from: :held, to: :purchased}}}
-
-    # seat stays purchased
-    Process.sleep(200)
-    assert {:purchased, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-  end
-
-  test "seat can transition from hold to available but still purchased", context do
-    %{show_seat: show_seat, customer: customer} = context
-
-    {:ok, pid} = ShowSeatState.start_link(show_seat, state_timeout: 200)
-
-    assert ShowSeatState.current_state(pid) == :available
-
-    assert ShowSeatState.hold(pid, customer) == {:ok, :held}
-    assert {:held, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-
-    # state transitions to available after timeout is triggered
-    Process.sleep(201)
-    assert {:available, %{current_customer: nil}} = ShowSeatState.get_state(pid)
-
-    # state can transition from available to held
-    assert ShowSeatState.hold(pid, customer) == {:ok, :held}
-    assert {:held, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-
-    # state can transition from held to purchased
-    Process.sleep(100)
-    assert ShowSeatState.purchase(pid, customer) == {:ok, :purchased}
-    assert {:purchased, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-
-    # seat stays purchased after the timeout
-    Process.sleep(300)
-    assert {:purchased, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-
-    # seat can't go back to hold
-    assert ShowSeatState.hold(pid, customer) == {:error, {:no_transition, %{from: :held, to: :purchased}}}
-
-    # seat stays purchased
-    Process.sleep(200)
-    assert {:purchased, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-  end
-
-  test "a customer can't purchase another customer's held seat", context do
-    %{show_seat: show_seat, customer: customer} = context
-
-    {:ok, pid} = ShowSeatState.start_link(show_seat, state_timeout: 200)
-
-    assert ShowSeatState.current_state(pid) == :available
-
-    assert ShowSeatState.hold(pid, customer) == {:ok, :held}
-    assert {:held, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-
-    ticket_scalper = %Customer{id: 3, first_name: "Ticket", last_name: "Scalper"}
-
-    # scalper can't purchase another customer's held ticket
-    assert ShowSeatState.purchase(pid, ticket_scalper) == {:error, {:no_transition, :unavailable}}
-    # ticket remains held by original customer
-    assert {:held, %{current_customer: customer}} = ShowSeatState.get_state(pid)
-
-    # state transitions back to available after timeout
-    Process.sleep(1_001)
     assert {:available, %{current_customer: nil}} = ShowSeatState.get_state(pid)
   end
 end
